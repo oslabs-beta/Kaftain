22c1ca3eabdb7ab9fd379516e834603a9ee2e210
Amrita Bahadur <amrita10in@yahoo.com>
2025-05-29 20:44:16 -0700

    Updated function fetchLagData to include consumerGroupName as a param

--- server/services/lagService.js
+++ server/services/lagService.js
@@ -1,31 +1,42 @@
 // Contains pure functions to fetch and parse Kafka lag metrics.
 import axios from 'axios';
 
+const url = 'http://52.52.97.230:9308/metrics'; //this is hardcoded but needs to updated
+const consumerGroupName = "test-consumer-static-value";
+
 // Fetches and parses lag data from the Kafka exporter
-export async function fetchLagData() {
-  const url = 'http://52.52.97.230:9308/metrics';
+export async function fetchLagData(url, consumerGroupName) {
   const response = await axios.get(url);
-  const metrics = response.data;
-  const lagLines = metrics
+    //console.log('response', response.data);
+
+    const metrics = response.data;
+    // Scrapes consumer lag lines from exporter
+    const lagLines = metrics
     .split('\n')
-    .filter((line) => line.startsWith('kafka_consumergroup_lag_sum'));
-  return lagLines
-    .map((line) => {
-      const match = line.match(
-        /kafka_consumergroup_lag_sum{([^}]*)} ([0-9.]+)/
-      );
-      if (!match) return null;
-      const matchArray = match[1].split(',');
-      const labels = matchArray.reduce((acc, pair) => {
-        const [key, val] = pair.split('=');
-        acc[key.trim()] = val.replace(/"/g, '');
-        return acc;
+    .filter(line =>
+      line.startsWith('kafka_consumergroup_lag_sum') &&
+      line.includes(`consumergroup="${consumerGroupName}"`)  //filter by consumer group
+    );
+    console.log('lagLines', lagLines);
+
+    const lagData = lagLines.map(line => {
+      const match = line.match(/kafka_consumergroup_lag_sum{([^}]*)} ([0-9.]+)/); //loops over lagLines array for each line that includes kafka_consumergroup_lag
+      if (!match) return null; //return null if no match is found
+
+      const matchArray = match[1].split(','); //selecting element at index 1 to extract topic data by splitting at ,
+
+      const labels = matchArray.reduce((acc, pair) => {     //looping over matchArray
+        const [key, val] = pair.split('=');         //splitting key value pairs at =
+        acc[key.trim()] = val.replace(/"/g, '');    //key.trim trims any white spaces on the key, /"/g replaces with an empty string
+        return acc; //
       }, {});
+
       return {
-        group: labels.consumergroup,
+        group: labels.consumergroup, //returns this object per match
         topic: labels.topic,
-        lag: parseFloat(match[2]),
+        lag: parseFloat(match[2])
       };
-    })
-    .filter(Boolean);
-}
+    }).filter(Boolean);
+  }
+
+  
