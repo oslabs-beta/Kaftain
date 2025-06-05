87e15ff7094f117920a2d2c4430291ac67f1e813
Austin Stubbs <austin.emuangstubbs@gmail.com>
2025-06-05 02:23:22 -0500

    Revert database username to 'root' in db.js configuration.

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 // const sequelize = new Sequelize('kaftain_db', 'root', 'kaftain',
-const sequelize = new Sequelize('kaftain_db', 'kaftain_user', 'kaftain', {
+const sequelize = new Sequelize('kaftain_db', 'root', 'kaftain', {
   host: process.env.DB_HOST || 'localhost', // or your MySQL host
   dialect: 'mysql',
   logging: (msg) => console.debug(`[SQL] ${msg}`), // Emit every raw SQL statement to console
